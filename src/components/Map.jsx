3c4f31bebf2d3198f6c43f24908549a0e3f4cd1e
emodeth <emodethistaken@gmail.com>
2023-08-22 22:33:25 +0300

    displaying city markers on the map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,9 +2,20 @@ import { useNavigate } from "react-router-dom";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import styles from "./Map.module.css";
 import { useState } from "react";
+import { useCities } from "../context/CitiesContext";
+
+const flagemojiToPNG = (flag) => {
+  var countryCode = Array.from(flag, (codeUnit) => codeUnit.codePointAt())
+    .map((char) => String.fromCharCode(char - 127397).toLowerCase())
+    .join("");
+  return (
+    <img src={`https://flagcdn.com/24x18/${countryCode}.png`} alt="flag" />
+  );
+};
 
 function Map() {
   const navigate = useNavigate();
+  const { cities } = useCities();
   const [mapPosition] = useState([41, 29]);
 
   return (
@@ -19,11 +30,17 @@ function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
-        <Marker position={mapPosition}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <span>{flagemojiToPNG(city.emoji)}</span>{" "}
+              <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </div>
   );
